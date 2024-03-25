817eb280e63806acc2f7181fa885d83451c10afa
PedroBahry <budokkanpvp10@Gmail.com>
2024-03-25 10:17:49 -0300

    EX11

---
+++ Exercicio18.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio18 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double nota1 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
+
+        double media = 3 / ((1 / nota1) + (1 / nota2) + (1 / nota3));
+
+        System.out.println("Media harmonica: " + media);
+
+        scanner.close();
+    }
+}
